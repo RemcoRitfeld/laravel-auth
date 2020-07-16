e155a62bfcdc8a5e74417c2872f56820d7ee6586
Remco Ritfeld <remcoritfeld@live.nl>
2020-07-16 17:21:04 +0200

    - Added login in navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -80,11 +80,52 @@
                             Contact
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{URL::to('/login')}}">
-                            Login
-                        </a>
-                    </li>
+
+                    {{-- Authentication Links --}}
+                    @guest
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Account
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{URL::to('/login')}}">
+                                    Login
+                                </a>
+
+                                <div class="dropdown-divider"></div>
+
+                                <a class="dropdown-item" href="{{URL::to('/register')}}">
+                                    Registreer
+                                </a>
+                            </div>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                @if ((Auth::User()->profile) && Auth::user()->profile->avatar_status == 1)
+                                    <img src="{{ asset(Auth::user()->profile->avatar) }}" alt="{{ Auth::user()->name }}" class="user-avatar-nav">
+                                @else
+                                    <div class="user-avatar-nav"></div>
+                                @endif
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item {{ Request::is('profile/'.Auth::user()->name, 'profile/'.Auth::user()->name . '/edit') ? 'active' : null }}" href="{{ url('/profile/'.Auth::user()->name) }}">
+                                    {!! trans('titles.profile') !!}
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
@@ -141,6 +182,36 @@
                     Login
                 </a>
             </li>
+            {{-- Authentication Links --}}
+            @guest
+                <li><a class="nav-link" href="{{ route('login') }}">{{ trans('titles.login') }}</a></li>
+                <li><a class="nav-link" href="{{ route('register') }}">{{ trans('titles.register') }}</a></li>
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        @if ((Auth::User()->profile) && Auth::user()->profile->avatar_status == 1)
+                            <img src="{{ Auth::user()->profile->avatar }}" alt="{{ Auth::user()->name }}" class="user-avatar-nav">
+                        @else
+                            <div class="user-avatar-nav"></div>
+                        @endif
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item {{ Request::is('profile/'.Auth::user()->name, 'profile/'.Auth::user()->name . '/edit') ? 'active' : null }}" href="{{ url('/profile/'.Auth::user()->name) }}">
+                            {!! trans('titles.profile') !!}
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
         </ul>
         <!-- Mobile Menu End -->
 
